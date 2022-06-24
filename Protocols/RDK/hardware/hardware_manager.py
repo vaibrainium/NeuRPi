7404159a49097282956e943065c1779cde7cb24e
vaibrainium <vaibhavt459@gmail.com>
2022-06-23 22:16:39 -0700

    hardware structure modification
    Separate 'hardware_manager' for each protocol. Taking inheritance from NeuRPi.hardware implementations

---
+++ Protocols/RDK/hardware/hardware_manager.py
@@ -0,0 +1,94 @@
+from NeuRPi.hardware.arduino import Arduino
+from NeuRPi.hardware.hardware import Hardware
+from NeuRPi.hardware.gpio import GPIO
+
+class HardwareManager(Hardware,Arduino):
+    def __init__(self):
+        self.hardware = {}
+
+    def init_hardware(self):
+        """
+        Initialize all hardware required by the task. Defined in HARDWARE dictionary in configuration file.
+        """
+        for group, container in self.config.HARDWARE.items():
+            if group == 'Arduino':
+                for name, properties in container.items():
+                    connect = properties['connection']
+                    exec(f"""self.hardware['{name}'] = Arduino(name='{name}', port='{connect['port']}', baudrate = {connect['baudrate']}, timeout={connect['timeout']})""")
+                    self.hardware['{name}'].connect
+
+        return self.hardware
+
+    def reward_left(self, volume):
+        """
+        Dispense 'volume' of Reward to Left spout
+
+        Arguments:
+            volume (float): Amount of reward to be given in ml
+
+        """
+        open_time = self.vol_to_dur(volume)
+        raise Warning("Reward delivery needs to be implemented")
+
+    def reward_right(self, volume):
+        """
+        Dispense 'volume' of Reward to Right spout
+
+        Arguments:
+            volume (float): Amount of reward to be given in ml
+
+        """
+        open_duration = self.vol_to_dur(volume)
+        raise Warning("Reward delivery needs to be implemented")
+
+    def vol_to_dur(self, volume):
+        """
+        Converting volume of reward to ms
+
+        Arguments:
+            volume (float): Amount of reward to be given in ml
+        """
+        open_duration = self.caliberation * volume
+        return open_duration
+
+    def reward_caliberation(self):
+        """
+        Method to caliberate reward.
+        """
+        self.caliberation = 30
+        raise Warning("TODO: Not Implemented")
+
+    def open_reward_indefinitely(self, spout):
+        """
+        Open reward spout indefinitely for priming purposes.
+        Arguments:
+            spout (str): 'Left','Right' or 'Center'
+        """
+        if spout == 'Left':
+            raise Warning("TODO: Not Implemented")
+        elif spout == 'Right':
+            raise Warning("TODO: Not Implemented")
+        elif spout == 'Center':
+            raise Warning("TODO: Not Implemented")
+        else:
+            raise Exception("Incorrect spout provided. Please provide from the following list: \n 'Left': For left spout"
+                            " \n 'Right': For right spout \n 'Center': For center spout")
+
+    def close_reward_indefinitely(self, spout):
+        """
+        Close reward spout indefinitely for priming purposes.
+        Arguments:
+            spout (str): 'Left','Right' or 'Center'
+        """
+        if spout == 'Left':
+            raise Warning("TODO: Not Implemented")
+        elif spout == 'Right':
+            raise Warning("TODO: Not Implemented")
+        elif spout == 'Center':
+            raise Warning("TODO: Not Implemented")
+        else:
+            raise Exception(
+                "Incorrect spout provided. Please provide from the following list: \n 'Left': For left spout"
+                " \n 'Right': For right spout \n 'Center': For center spout")
+
+
