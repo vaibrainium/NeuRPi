a192ae130fd4907d9bbef855b1fdf0177c584ac2
vaibrainium <vaibhavt459@gmail.com>
2022-06-30 19:21:11 -0700

    Implement working frame_queue without added functionality

--- NeuRPi/stimulus/display.py
+++ NeuRPi/stimulus/display.py
@@ -8,7 +8,7 @@ class Display():
     Base class for video stimulus display. Defines display properties.
     """
 
-    def __init__(self, configuration=None):
+    def __init__(self, configuration=None, pygame=pygame):
         """
         Inputs configuration file
         """
@@ -17,11 +17,11 @@ class Display():
         self.frame_rate = self.stim_config.display.frame_rate
         self.flags = eval(self.stim_config.display.flags)  # Converting flags from string to method name
         self.vsync = self.stim_config.display.vsync
-        self.clock = pygame.time.Clock()
+        self.pygame = pygame
+        self.clock = self.pygame.time.Clock()
         self.screen = {}
 
-        self._courier_props = OmegaConf.create(self.stim_config.courier_handle)
-        self.loaded_audio = {}
+        self.audio = {}
         self.gen_visual = {}
         self._start()
 
@@ -29,95 +29,87 @@ class Display():
 
     def _start(self):
         # Initialize all screens with black background
-        pygame.init()
-        pygame.mixer.init()
+        self.pygame.init()
+        self.pygame.mixer.init()
+        self.pygame.font.init()
+        self.font = self.pygame.font.SysFont("Arial", 20)
+
         if self.stim_config.display.num_screens == 1:
-            self.screen[0] = pygame.display.set_mode(self.window_size, flags=self.flags,
-                                                     display=self.stim_config.display.screen, vsync=self.vsync)
+            self.screen[0] = self.pygame.display.set_mode(self.window_size, flags=self.flags,
+                                                     display=eval(self.stim_config.display.screen), vsync=self.vsync)
             self.screen[0].fill((0, 0, 0))
         else:
             for screen in range(self.stim_config.display.num_screens):
                 exec(
-                    f"""self.screen[{screen}] = pygame.display.set_mode(self.window_size, flags=self.flags, display=screen, vsync=self.vsync)""")
+                    f"""self.screen[{screen}] = self.pygame.display.set_mode(self.window_size, flags=self.flags, display=screen, vsync=self.vsync)""")
                 exec(f"""self.screen[{screen}].fill((0,0,0))""")
         self._update()
-        # Initialize and load audios
-        self.load_properties()
 
-    def load_properties(self):
-        for key, val in self._courier_props.items():
+        self.gather_media()
+
+    def gather_media(self):
+        for key, val in self.stim_config.courier_handle.items():
             if key not in ['tag', 'type']:
+                # exec(f"self.{key} = dict()")
                 if val.visual.properties.load:
-                    pass
+                    self.load_videos(key, val.audio.properties.load)
                 if val.audio.properties.load:
                     self.load_audios(key, val.audio.properties.load)
 
-    def load_audios(self, key, val):
+    def load_videos(self, key, val):
         if isinstance(val, DictConfig):
-            self.loaded_audio[key] = {}
+            self.audio[key] = {}
             for key2, paths in val.items():
                 for ind, file in enumerate(paths):
                     temp_list = []
                     try:
-                        temp_list.append(pygame.mixer.Sound(file))
+                        pass
+                        # temp_list.append(pygame.mixer.Sound(file))
                     except:
                         raise Warning(f'Could not initialize {file} in {key2} in {key}')
                     finally:
-                        self.loaded_audio[key][key2] = temp_list
+                        self.audio[key][key2] = temp_list
         else:
             for ind, file in enumerate(val):
                 temp_list = []
                 try:
-                    temp_list.append(pygame.mixer.Sound(file))
+                    pass
+                    # temp_list.append(pygame.mixer.Sound(file))
                 except:
                     raise Warning(f'Could not initialize {file} in {key}')
                 finally:
-                    self.loaded_audio[key] = temp_list
-
-
-
-    # def gather_properties(self, path_dict):
-    #     for key, val in path_dict.items():
-    #         if key not in ['tag', 'type']:
-    #             exec(f"self.{key} = dict()")
-    #             if val.audio.properties.load:
-    #                 pass
-    #             if val.audio.properties.generate:
-    #                 self.generate_visuals(key, val)
-    #             if val.audio.properties.load:
-    #                 self.load_audios(key, val.audio.properties.load)
-    #             if val.audio.properties.generate:
-    #                 self.generate_audios(key, val.audio.properties.load)
-
-    def load_visuals(self, key, val):
-        pass
+                    self.audio[key] = temp_list
 
     def load_audios(self, key, val):
         if isinstance(val, DictConfig):
-            self.loaded_audio[key] = {}
+            self.audio[key] = {}
             for key2, paths in val.items():
                 for ind, file in enumerate(paths):
                     temp_list = []
                     try:
-                        temp_list.append(pygame.mixer.Sound(file))
+                        temp_list.append(self.pygame.mixer.Sound(file))
                     except:
                         raise Warning(f'Could not initialize {file} in {key2} in {key}')
                     finally:
-                        self.loaded_audio[key][key2] = temp_list
+                        self.audio[key][key2] = temp_list
         else:
             for ind, file in enumerate(val):
                 temp_list = []
                 try:
-                    temp_list.append(pygame.mixer.Sound(file))
+                    temp_list.append(self.pygame.mixer.Sound(file))
                 except:
                     raise Warning(f'Could not initialize {file} in {key}')
                 finally:
-                    self.loaded_audio[key] = temp_list
-
+                    self.audio[key] = temp_list
 
     def _update(self):
-        pygame.display.update()
-        pygame.event.pump()
+        self.pygame.display.update()
+        self.clock.tick(self.frame_rate)
+        self.pygame.event.pump()
+
+    def _display_fps(self, screen):
+        fps = self.font.render(str(int(self.clock.get_fps())), 1, pygame.Color("coral"))
+        self.screen[screen].blit(fps, (1900, 1000))
 
     def _end(self):
-        pygame.quit()
+        self.pygame.quit()
--- NeuRPi/stimulus/stim_window.py
+++ NeuRPi/stimulus/stim_window.py
@@ -1,5 +1,8 @@
 import threading
 from NeuRPi.stimulus.display import Display
+from queue import Queue, Empty
+import time
+from RandomMotion import DotMotionStim
 
 class StimWindow(Display):
     """
@@ -11,12 +14,22 @@ class StimWindow(Display):
         self.courier = courier
         self.message = {}
         self.prev_message = {}
-        self.stim_block = threading.Event()
-        self.stim_block.clear()
+        self.render_block = threading.Event()
+        self.render_block.clear()
         self.stim_config = configuration.STIMULUS
+        self.frame_queue_size = 100
+        self.frame_queue = Queue(maxsize=self.frame_queue_size)
         super(StimWindow, self).__init__(configuration=configuration)
 
-        self.thread = threading.Thread(target=self.get_messages(), args=[], daemon=True).start()
+
+        self.Dots = DotMotionStim()
+        self.Dots.newStimulus(100, 1)
+        # self.Dots.vel = self.stim_config.
+        # self.thread = threading.Thread(target=self.run, args=[], daemon=False).start()
+        self.thread = threading.Thread(target=self.render_visual, args=[], daemon=False).start()
+
+
+        self.run()
 
     def get_messages(self):
         while True:
@@ -24,15 +37,44 @@ class StimWindow(Display):
                 self.message = self.courier.get()
                 self.stim_block.set()
 
-
     def run(self):
-        # self.courier_map = self.stim_config.courier_handle
         while True:
-            self.stim_block.wait()
-            self.stim_block.clear()
-            # properties = self.courier_map.get(self.message)
-            # function = eval('self.' + properties.function)
-            # function(is_static=properties.is_static)
+            if self.frame_queue.qsize() < self.frame_queue_size*0.2: # not self.frame_queue.full():
+                for _ in range(self.frame_queue_size*70):
+                    func = self.draw_stim
+                    screen = 0
+                    pars = self.next_frame_stim()
+                    self.frame_queue.put([func, screen, pars])
+
+
+
+    def render_visual(self):
+        while True:
+            (func, screen, pars) = self.frame_queue.get()
+            func(screen=screen, pars=pars)
+            self._display_fps(screen)
+            self._update()
+
+
+
+
+    def next_frame_stim(self):
+        self.Dots.moveDots(self.frame_rate)
+        pars = {'ndots': self.Dots.nDots,
+                'xpos': self.Dots.x,
+                'ypos': self.Dots.y,
+                'radius': [self.Dots.radius] * self.Dots.nDots,
+                'color': [self.Dots.color] * self.Dots.nDots,
+                }
+        return pars
+
+    def draw_stim(self, screen=0, pars = {}):
+        self.screen[screen].fill((0,0,0))
+        for ind in range(len(pars['xpos'])):
+            self.pygame.draw.circle(self.screen[screen], pars['color'][ind], (pars['xpos'][ind], pars['ypos'][ind]), pars['radius'][ind])
+
+
+
     #
     # def exec_func(self, function=None, is_static={'video': False, 'audio': False}, screen=0, *args, **kwargs):
     #     video, audio = [0, 0]
@@ -53,13 +95,13 @@ class StimWindow(Display):
 
 
 
-    # def fixation(self, screen=0):
-    #     self.screen[screen].fill(eval(self.stim_config.fixation.background))
-    #     if self.stim_config.fixation.audio:
-    #         try:
-    #             self.audio['fixation_onset'].play()
-    #         except:
-    #             raise Warning('fixation_onset audio path not set')
+    def fixation(self, screen=0):
+        self.screen[screen].fill(eval(self.stim_config.fixation.background))
+        if self.stim_config.fixation.audio:
+            try:
+                self.audio['fixation_onset'].play()
+            except:
+                raise Warning('fixation_onset audio path not set')
     #
     # def reinforcement(self, screen=0, outcome=None):
     #     if self.stim_config.reinfocement.audio:
