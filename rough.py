82e872e03f13dceaed2ac57bbfd86116d49c67a5
Vaibhav Thakur <vaibhavt459@gmail.com>
2023-01-05 16:46:12 -0800

    starting_server_work

--- rough.py
+++ rough.py
@@ -1,45 +1,45 @@
-# import time
-
-# if __name__ == "__main__":
-
-#     from NeuRPi.agents import test_pilot, test_terminal
-
-#     test_pilot.main()
-#     # test_terminal.main()
-#     pass
-
-
-import importlib
-import threading
-
-# from protocols.RDK.data_model.subject import Subject
-from protocols.RDK.tasks.dynamic_training_rt import dynamic_training_rt
+import time
 
 if __name__ == "__main__":
-    stage_block = threading.Event()
-    running = threading.Event()
-    quitting = threading.Event()
 
-    value = {
-        "task_module": "RDK",
-        "task_phase": "dynamic_training_rt",
-        "subject_id": "PSUIM4",
-    }
+    from NeuRPi.agents import test_pilot, test_terminal
 
-    # Importing protocol function/class object using importlib
-    task = dynamic_training_rt(stage_block, **value)
-    running.set()
+    test_pilot.main()
+    # test_terminal.main()
+    pass
 
-    while True:
-        stage = next(task.stages)
-        print(stage)
-        data = stage()
 
-        stage_block.wait()
+# import importlib
+# import threading
 
-        # Has trial ended?
-        if "TRIAL_END" in data.keys():
-            running.wait()  # If paused, waiting for running event set?
-            if quitting.is_set():  # is quitting event set?
-                task.end_session()
-                break  # Won't quit if the task is paused.
+# # from protocols.RDK.data_model.subject import Subject
+# from protocols.RDK.tasks.dynamic_training_rt import dynamic_training_rt
+
+# if __name__ == "__main__":
+#     stage_block = threading.Event()
+#     running = threading.Event()
+#     quitting = threading.Event()
+
+#     value = {
+#         "task_module": "RDK",
+#         "task_phase": "dynamic_training_rt",
+#         "subject_id": "PSUIM4",
+#     }
+
+#     # Importing protocol function/class object using importlib
+#     task = dynamic_training_rt(stage_block, **value)
+#     running.set()
+
+#     while True:
+#         stage = next(task.stages)
+#         print(stage)
+#         data = stage()
+
+#         stage_block.wait()
+
+#         # Has trial ended?
+#         if "TRIAL_END" in data.keys():
+#             running.wait()  # If paused, waiting for running event set?
+#             if quitting.is_set():  # is quitting event set?
+#                 task.end_session()
+#                 break  # Won't quit if the task is paused.
