eb0ec7d9b9fae5060172b709e83d32a6f592e38c
Vaibhav Thakur <vaibhavt459@gmail.com>
2024-02-06 17:52:12 -0800

    min passive reward to 1ul

--- protocols/random_dot_motion/rt_dynamic_training/session_manager.py
+++ protocols/random_dot_motion/rt_dynamic_training/session_manager.py
@@ -263,7 +263,7 @@ class SessionManager:
         if self.training_type < 2 and self.outcome=="noresponse":
             self.reinforcement_duration = self.reinforcement_duration_function["correct"](self.response_time)
             self.trial_reward = self.full_reward_volume / 2
-            self.trial_reward = max(self.trial_reward, 1.5) # making sure reward is not below 1.5 ul
+            self.trial_reward = max(self.trial_reward, 1) # making sure reward is not below 1.5 ul
             # msg to stimulus
             stage_stimulus_args["outcome"] = "correct"
 
